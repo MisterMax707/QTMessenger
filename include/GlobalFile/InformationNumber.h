af48f92f43fe9a38758e301c5931bff7120441e1
Lovytug <8751290@bk.ru>
2025-01-28 15:56:44 +0300

    Добавление функционала : операторы перегрезуки, для класса слов и айди

--- include/GlobalFile/InformationNumber.h
+++ include/GlobalFile/InformationNumber.h
@@ -1,26 +1,60 @@
 #pragma once
-#include <string>
+#include "../include/QtCore/qstring.h"
 
 class InformationNumber
 {
 private:
-    std::string allNumber;
+    QString allNumber;
 
 public:
     InformationNumber() : allNumber("") {}
 
-    InformationNumber(const std::string& str) : allNumber(str) {}
+    InformationNumber(const QString& str) : allNumber("#" + str) {}
 
-    InformationNumber operator+(const std::string& str) const
+    InformationNumber operator=(const QString& str)
+    {
+        allNumber = "#" + str;
+        return *this;
+    }
+
+    InformationNumber operator+(const QString& str) const
     {
         return InformationNumber(allNumber + "#" + str);
     }
 
-    InformationNumber operator+=(const std::string& str)
+    InformationNumber operator+=(const QString& str)
     {
-        allNumber += str;
+        allNumber = allNumber + "#" + str;
         return *this;
+    }
+
+    template<typename... Args>
+    InformationNumber operator+(Args... args) const
+    {
+        return (*this + ... + args);
+    }
+
+    QString getAllNumber() const
+    {
+        return allNumber;
     }
 };
 
+// Перегрузка оператора + для QString и InformationWord
+InformationNumber operator+(const QString& str, const InformationNumber& infoNumber)
+{
+    return InformationNumber("#" + str + "#" + infoNumber.getAllNumber());
+}
+
+// Перегрузка оператора + для InformationWord и QString
+InformationNumber operator+(const InformationNumber& infoNumber, const QString& str)
+{
+    return InformationNumber("#" + infoNumber.getAllNumber() + "#" + str);
+}
+
+// Перегрузка оператора + для двух InformationWord
+InformationNumber operator+(const InformationNumber& infoNumber1, const InformationNumber& infoNumber2)
+{
+    return InformationNumber("#" + infoNumber1.getAllNumber() + "#" + infoNumber2.getAllNumber());
+}
 
--- include/GlobalFile/InformationWord.h
+++ include/GlobalFile/InformationWord.h
@@ -1,25 +1,61 @@
 #pragma once
-#include <string>
+#include "../include/QtCore/qstring.h"
 
 class InformationWord
 {
 private:
-    std::string allWord;
+    QString allWord;
 
 public:
     InformationWord() : allWord("") {}
 
-    InformationWord(const std::string& str) : allWord(str) {}
+    InformationWord(const QString& str) : allWord("|" + str) {}
 
-    InformationWord operator+(const std::string& str) const
+    InformationWord operator=(const QString& str)
+    {
+        allWord = "|" + str;
+        return *this;
+    }
+
+    InformationWord operator+(const QString& str) const
     {
         return InformationWord(allWord + "|" + str);
     }
 
-    InformationWord operator+=(const std::string& str)
+    InformationWord operator+=(const QString& str)
     {
-        allWord += str;
+        allWord = allWord + "|" + str;
         return *this;
     }
+
+    template<typename... Args>
+    InformationWord operator+(Args... args) const
+    {
+        return (*this + ... + args);
+    }
+
+    QString getAllWord() const
+    {
+        return allWord;
+    }
 };
+
+// Перегрузка оператора + для QString и InformationWord
+InformationWord operator+(const QString& str, const InformationWord& infoWord)
+{
+    return InformationWord("|" + str + "|" + infoWord.getAllWord());
+}
+
+// Перегрузка оператора + для InformationWord и QString
+InformationWord operator+(const InformationWord& infoWord, const QString& str)
+{
+    return InformationWord("|" + infoWord.getAllWord() + "|" + str);
+}
+
+// Перегрузка оператора + для двух InformationWord
+InformationWord operator+(const InformationWord& infoWord1, const InformationWord& infoWord2)
+{
+    return InformationWord("|" + infoWord1.getAllWord() + "|" + infoWord2.getAllWord());
+}
+
 
